51aa1b76e9be795a5070c1ee6336fc6993599b6b
zeraatifatemeh98-cloud <zeraatifatemeh98@gmail.com>
2025-12-25 16:10:57 +0330

    Update geminiService.ts

--- geminiService.ts
+++ geminiService.ts
@@ -2,7 +2,7 @@
 import { GoogleGenAI, Type } from "@google/genai";
 import { AnalysisData } from "./types";
 
-const ai = new GoogleGenAI({ apiKey: process.env.API_KEY || "" });
+const ai = new GoogleGenAI({apiKey: import.meta.env.VITE_GEMINI_API_KEY || "" });
 
 export const analyzeScribble = async (base64Image: string): Promise<AnalysisData> => {
   const model = "gemini-3-flash-preview";
